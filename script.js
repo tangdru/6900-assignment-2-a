dec8bf2383558de1f0959f1aecaa0c2e98fb918f
tangdru <tangdru@gmail.com>
2016-02-28 23:40:08 -0500

    filtered by decade

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ d3.csv('data/hubway_trips_reduced.csv',parse,dataLoaded);
 
 function dataLoaded(err,rows){
 
-console.log(rows);
+//console.log(rows);
 
 //create crossfilter
 var cfilter = crossfilter(rows);
@@ -16,15 +16,13 @@ var start2012 = new Date ('January 01, 2012 00:00:00'),
     
 var tripsByYear = cfilter.dimension(function(d){return d.startTime});
     tripsIn2012 = tripsByYear.filterRange([start2012,end2012]).top(Infinity);
-//    console.log(start2012);
-//    console.log(end2012);
     console.log("Total trips in 2012", tripsIn2012.length);
 
 //filter by male + registered users   
-var tripsByGender = cfilter.dimension(function(rows){return rows.gender});
+var tripsByGender = cfilter.dimension(function(d){return d.gender});
     tripsMale = tripsByGender.filter("Male").top(Infinity);
     //filter by registered users 
-var tripsByRegistration = cfilter.dimension(function(rows){return rows.reg});
+var tripsByRegistration = cfilter.dimension(function(d){return d.reg});
     tripsRegistered = tripsByRegistration.filter("Registered").top(Infinity);
     console.log("Total trips in 2012 by Male Registered Users", tripsRegistered.length);   
     tripsByGender.filter(null); 
@@ -32,31 +30,32 @@ var tripsByRegistration = cfilter.dimension(function(rows){return rows.reg});
 
     
 //filter by trips starting at station 5    
-var tripsByStation = cfilter.dimension(function(rows){return rows.startStation});
+var tripsByStation = cfilter.dimension(function(d){return d.startStation});
     tripsByStation5 = tripsByStation.filter("5").top(Infinity);
     console.log("Total trips in 2012 from station 5", tripsByStation5.length);
     tripsByStation.filter(null);
     tripsByYear.filter(null);
     
 //top 50 trips regardless of other variables     
-var tripsByDuration = cfilter.dimension(function(rows){return rows.duration});
+var tripsByDuration = cfilter.dimension(function(d){return d.duration});
     top50ByDuration = tripsByDuration.top(50);
     console.log("Top 50 trips by duration ", top50ByDuration);
     
-
+//zero'ing out my filters
 tripsByYear.filter(null);
 tripsByGender.filter(null);
 tripsByRegistration.filter(null);
 tripsByStation.filter(null);
 tripsByDuration.filter(null);
     
-//trips into 3 grouping     
-var tripsByTime = cfilter.dimension(function(rows){return rows.startTime}); 
-    tripsByYearGroup = tripsByTime.group(function(rows){return rows.getFullYear(2011,2012,2013)});
-    console.log(tripsByYearGroup);
-    
-
 //trips by 10 year age increments
+var tripsByAge = cfilter.dimension(function(d){return d.age});
+var tripsByAgeGroups = tripsByAge.group(function(d){return Math.floor(d/10)});
+
+var decade = tripsByAgeGroups.all();
+    console.log("Trips separated by age group by decade ", decade)
+
+
     
     
     
